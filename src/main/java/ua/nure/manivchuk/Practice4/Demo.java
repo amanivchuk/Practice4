4eb7d465fd2fef2d378133feb09b538e2885c3a5
Alexandr Manivchuk <amanivchuk@gmail.com>
2017-11-21 23:01:22 +0200

    Initial commit

---
+++ src/main/java/ua/nure/manivchuk/Practice4/Demo.java
@@ -0,0 +1,14 @@
+package ua.nure.manivchuk.Practice4;
+
+import ua.nure.manivchuk.Practice4.task1.Task1;
+
+/**
+ * Created by Lenovo on 11/21/2017.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("============= Part 1 ============");
+        Task1 task1 = new Task1();
+        System.out.println(task1.showTextFromFile());
+    }
+}
---
+++ src/main/java/ua/nure/manivchuk/Practice4/util/ReadFile.java
@@ -0,0 +1,29 @@
+package ua.nure.manivchuk.Practice4.util;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nec on 15.11.17.
+ */
+public class ReadFile {
+
+    public static StringBuffer readFile(String fileName){
+        List<String> stringList = new ArrayList<>();
+        StringBuffer buffer = new StringBuffer();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "utf-8"))) {
+            String currentLine;
+            while((currentLine = reader.readLine()) != null){
+                buffer.append(currentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return buffer;
+    }
+}
